18a69ad1baffba0f63a650b55c77ca8d78e584cb
Madhurima29das <103365638+Madhurima29das@users.noreply.github.com>
2022-04-11 20:51:52 +0530

    Add files via upload

---
+++ intro program  ( P).py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 20:43:36 2022
+
+@author: mdas8
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 16:45:56 2021
+@author: pc
+"""
+
+V = [1,2,3,4,5]
+print(V)
+
+import matplotlib.pyplot as plt
+plt.plot(V)
